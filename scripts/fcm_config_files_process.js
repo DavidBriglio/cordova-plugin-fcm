1ae96a60b51d44a5e0a070403c594d29e09582db
David Briglio <david.briglio@versaterm.com>
2017-12-12 15:20:28 -0500

    Fixed paths for cordova-android-7.x
    + Cleanup

--- scripts/fcm_config_files_process.js
+++ scripts/fcm_config_files_process.js
@@ -4,13 +4,13 @@
 var fs = require('fs');
 
 var getValue = function(config, name) {
-    var value = config.match(new RegExp('<' + name + '>(.*?)</' + name + '>', "i"))
+    var value = config.match(new RegExp('<' + name + '>(.*?)</' + name + '>', "i"));
     if(value && value[1]) {
-        return value[1]
+        return value[1];
     } else {
-        return null
+        return null;
     }
-}
+};
 
 function fileExists(path) {
   try  {
@@ -30,8 +30,8 @@ function directoryExists(path) {
   }
 }
 
-var config = fs.readFileSync("config.xml").toString()
-var name = getValue(config, "name")
+var config = fs.readFileSync("config.xml").toString();
+var name = getValue(config, "name");
 
 if (directoryExists("platforms/ios")) {
 	var path = "GoogleService-Info.plist";
@@ -39,7 +39,7 @@ if (directoryExists("platforms/ios")) {
     if (fileExists( path )) {
       try {
         var contents = fs.readFileSync(path).toString();
-        fs.writeFileSync("platforms/ios/" + name + "/Resources/GoogleService-Info.plist", contents)
+        fs.writeFileSync("platforms/ios/" + name + "/Resources/GoogleService-Info.plist", contents);
       } catch(err) {
         process.stdout.write(err);
       }
@@ -58,24 +58,24 @@ if (directoryExists("platforms/android")) {
         fs.writeFileSync("platforms/android/google-services.json", contents);
 
         var json = JSON.parse(contents);
-        var strings = fs.readFileSync("platforms/android/res/values/strings.xml").toString();
+        var strings = fs.readFileSync("platforms/android/app/src/main/res/values/strings.xml").toString();
+        
+        // strip non-default value
+        strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)</string>', "i"), '');
 
         // strip non-default value
-        strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)</string>', "i"), '')
-
-        // strip non-default value
-        strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)</string>', "i"), '')
+        strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)</string>', "i"), '');
 
         // strip empty lines
-        strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1')
+        strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1');
 
         // replace the default value
-        strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)</string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>')
+        strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)</string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>');
 
         // replace the default value
-        strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)</string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>')
+        strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)</string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>');
 
-        fs.writeFileSync("platforms/android/res/values/strings.xml", strings);
+        fs.writeFileSync("platforms/android/app/src/main/res/values/strings.xml", strings);
       } catch(err) {
         process.stdout.write(err);
       }
